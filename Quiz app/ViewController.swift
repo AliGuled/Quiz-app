a3424f1f968f6a98dda226a21104e8461ec9f8dd
Guled Ali <ae7244xg@t3000-1-2.local>
2019-01-15 09:57:48 -0600

    Initial Commit

---
+++ Quiz app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz app
+//
+//  Created by Guled Ali on 1/15/19.
+//  Copyright © 2019 Guled Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
